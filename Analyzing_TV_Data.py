113f65345c11c7b782a38b25e3a979570587fe4c
Joscotea <josephayomj@gmail.com>
2024-01-26 16:42:25 +0000

    Loaded datasets into dataframes

--- Analyzing_TV_Data.py
+++ Analyzing_TV_Data.py
@@ -1,3 +1,8 @@
 #Codes to analyze TV Data
+# Import pandas
+import pandas as pd
 
-#Plots
+# Load the CSV data into DataFrames
+super_bowls = pd.read_csv('datasets/super_bowls.csv')
+tv = pd.read_csv('datasets/tv.csv')
+halftime_musicians = pd.read_csv('datasets/halftime_musicians.csv')
