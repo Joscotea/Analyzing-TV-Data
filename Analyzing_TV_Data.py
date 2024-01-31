7cf0f5dc62442fb170ca925c25753e33454a4a42
Joscotea <josephayomj@gmail.com>
2024-01-31 23:44:12 +0000

    Display the closest game(s) and biggest blowouts

--- Analyzing_TV_Data.py
+++ Analyzing_TV_Data.py
@@ -40,3 +40,7 @@ plt.hist(super_bowls.difference_pts)
 plt.xlabel('Point Difference')
 plt.ylabel('Number of Super Bowls')
 plt.show()
+
+# Display the closest game(s) and biggest blowouts
+print(super_bowls[super_bowls['difference_pts'] == 1])
+print(super_bowls[super_bowls['difference_pts'] > 35])
