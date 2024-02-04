0b02f334a371fb9227875af385d4ed8a7a07f2ea
Joscotea <josephayomj@gmail.com>
2024-02-04 15:51:37 +0000

    Join game and tv data

--- Analyzing_TV_Data.py
+++ Analyzing_TV_Data.py
@@ -44,3 +44,6 @@ plt.show()
 # Display the closest game(s) and biggest blowouts
 print(super_bowls[super_bowls['difference_pts'] == 1])
 print(super_bowls[super_bowls['difference_pts'] > 35])
+
+# Join game and TV data, filtering out SB I because it was split over two networks
+games_tv = pd.merge(tv[tv['super_bowl'] > 1], super_bowls, on='super_bowl')
