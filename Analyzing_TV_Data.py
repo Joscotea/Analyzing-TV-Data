2dd18727f9f0153e146f30c978b6c3467de3320d
Joscotea <josephayomj@gmail.com>
2024-02-04 15:46:59 +0000

    Plot distribution of point differences

--- Analyzing_TV_Data.py
+++ Analyzing_TV_Data.py
@@ -44,3 +44,10 @@ plt.show()
 # Display the closest game(s) and biggest blowouts
 print(super_bowls[super_bowls['difference_pts'] == 1])
 print(super_bowls[super_bowls['difference_pts'] > 35])
+
+# Plot a histogram of point differences
+plt.hist(super_bowls.difference_pts)
+plt.xlabel('Point Difference')
+plt.ylabel('Number of Super Bowls')
+plt.show()
+
